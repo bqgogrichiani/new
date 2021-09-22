aad00431e90e8f7a5a57b641bd41da0f039fd12f
bqgogrichiani <74893988+bqgogrichiani@users.noreply.github.com>
2021-09-22 13:37:00 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks =document.querySelector('.navbar__menu')
+const navLogo =document.querySelector('navbar__logo')
 
 // Display Mobile Menu
 
@@ -9,3 +10,37 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click' , mobileMenu);
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const servicesMenu = document.querySelector('#services-page')
+    let scrollPos = window.scrollY
+    console.log(scrollPos);
+
+    if (window.innerWidth > 960 && scrollPos < 600){
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }
+        else if (window.innerWidth > 960 && scrollPos < 1400){
+            aboutMenu.classList.add('highlight')
+            homeMenu.classList.remove('highlight')
+            servicesMenu.classList.remove('highlight')
+            return
+        }
+            else if (window.innerWidth > 960 && scrollPos < 2345){
+                aboutMenu.classList.remove('highlight')
+                servicesMenu.classList.add('highlight')    
+                return
+        }
+        if((elem && window.innerWidth < 960 && scrollPos < 600) || elem){
+            elem.classList.remove('highlight')
+        }
+    }
+
+    window.addEventListener('scroll' , highlightMenu);
+    window.addEventListener('click' , highlightMenu);
+    
+
